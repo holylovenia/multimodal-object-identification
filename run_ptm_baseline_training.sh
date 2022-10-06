ca87a48caf525710120ce67f342e662abdf01abc
Holy Lovenia <holy.lovenia@gmail.com>
2022-10-06 16:45:20 +0800

    Minor naming change

--- run_ptm_baseline_training.sh
+++ run_ptm_baseline_training.sh
@@ -6,7 +6,7 @@ CUDA_VISIBLE_DEVICES=0 python train_model.py \
     --train_file "/home/holy/projects/ambiguous-mm-dialogue/preprocessed_data/ambiguous_candidates/simmc2.1_ambiguous_candidates_dstc11_train.json" \
     --dev_file "/home/holy/projects/ambiguous-mm-dialogue/preprocessed_data/ambiguous_candidates/simmc2.1_ambiguous_candidates_dstc11_dev.json" \
     --devtest_file "/home/holy/projects/ambiguous-mm-dialogue/preprocessed_data/ambiguous_candidates/simmc2.1_ambiguous_candidates_dstc11_devtest.json" \
-    --result_save_path "/home/holy/projects/ambiguous-mm-dialogue/results/" \
+    --result_save_path "/home/holy/projects/ambiguous-mm-dialogue/results/ambiguous-candidates/gpt2" \
     --visual_feature_path "/home/holy/projects/ambiguous-mm-dialogue/preprocessed_data/visual_features/visual_features_resnet50_simmc2.1.pt" \
     --visual_feature_size 516 \
-	--backbone bert --use_gpu --num_epochs 10 --batch_size 16 --max_turns 2
+	--backbone gpt2 --use_gpu --num_epochs 10 --batch_size 16 --max_turns 2
