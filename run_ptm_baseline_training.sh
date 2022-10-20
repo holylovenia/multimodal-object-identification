4c38622c984752307c4fce8d0ee1d52376296240
Holy Lovenia <holy.lovenia@gmail.com>
2022-10-20 12:10:27 +0800

    Add SIMMC 2.1 baseline training commands

--- run_ptm_baseline_training.sh
+++ run_ptm_baseline_training.sh
@@ -2,6 +2,26 @@ cd simmc2/model/ambiguous_candidates/
 
 pwd
 
+CUDA_VISIBLE_DEVICES=0 python train_model.py \
+    --train_file "/home/holy/projects/ambiguous-mm-dialogue/preprocessed_data/ambiguous_candidates/simmc2.1_ambiguous_candidates_dstc11_train.json" \
+    --dev_file "/home/holy/projects/ambiguous-mm-dialogue/preprocessed_data/ambiguous_candidates/simmc2.1_ambiguous_candidates_dstc11_dev.json" \
+    --devtest_file "/home/holy/projects/ambiguous-mm-dialogue/preprocessed_data/ambiguous_candidates/simmc2.1_ambiguous_candidates_dstc11_devtest.json" \
+    --result_save_path "/home/holy/projects/ambiguous-mm-dialogue/results/ambiguous-candidates/detr/gpt2" \
+    --visual_feature_path "/home/holy/projects/ambiguous-mm-dialogue/preprocessed_data/visual_features/visual_features_resnet50_simmc2.1.pt" \
+    --visual_feature_size 516 \
+	--backbone gpt2 --use_gpu --num_epochs 10 --batch_size 16 --max_turns 2
+
+# CUDA_VISIBLE_DEVICES=1 python train_model.py \
+#     --train_file "/home/holy/projects/ambiguous-mm-dialogue/preprocessed_data/ambiguous_candidates/simmc2.1_ambiguous_candidates_dstc11_train.json" \
+#     --dev_file "/home/holy/projects/ambiguous-mm-dialogue/preprocessed_data/ambiguous_candidates/simmc2.1_ambiguous_candidates_dstc11_dev.json" \
+#     --devtest_file "/home/holy/projects/ambiguous-mm-dialogue/preprocessed_data/ambiguous_candidates/simmc2.1_ambiguous_candidates_dstc11_devtest.json" \
+#     --result_save_path "/home/holy/projects/ambiguous-mm-dialogue/results/ambiguous-candidates/detr/gpt2" \
+#     --visual_feature_path "/home/holy/projects/ambiguous-mm-dialogue/preprocessed_data/visual_features/visual_features_resnet50_simmc2.1.pt" \
+#     --visual_feature_size 516 \
+# 	--backbone bert --use_gpu --num_epochs 10 --batch_size 16 --max_turns 2
+
+## DETR
+
 # CUDA_VISIBLE_DEVICES=0 python train_model.py \
 #     --train_file "/home/holy/projects/ambiguous-mm-dialogue/preprocessed_data/ambiguous_candidates/simmc2.1_ambiguous_candidates_dstc11_train.json" \
 #     --dev_file "/home/holy/projects/ambiguous-mm-dialogue/preprocessed_data/ambiguous_candidates/simmc2.1_ambiguous_candidates_dstc11_dev.json" \
@@ -12,11 +32,11 @@ pwd
 # 	--backbone gpt2 --use_gpu --num_epochs 10 --batch_size 16 --max_turns 2
 
 
-CUDA_VISIBLE_DEVICES=0 python train_model.py \
-    --train_file "/home/holy/projects/ambiguous-mm-dialogue/preprocessed_data/ambiguous_candidates/simmc2.1_ambiguous_candidates_dstc11_train.json" \
-    --dev_file "/home/holy/projects/ambiguous-mm-dialogue/preprocessed_data/ambiguous_candidates/simmc2.1_ambiguous_candidates_dstc11_dev.json" \
-    --devtest_file "/home/holy/projects/ambiguous-mm-dialogue/preprocessed_data/ambiguous_candidates/simmc2.1_ambiguous_candidates_dstc11_devtest.json" \
-    --result_save_path "/home/holy/projects/ambiguous-mm-dialogue/results/ambiguous-candidates/detr/bert" \
-    --visual_feature_path "/home/holy/projects/ambiguous-mm-dialogue/preprocessed_data/visual_features/visual_features_detr.pt" \
-    --visual_feature_size 260 \
-	--backbone bert --use_gpu --num_epochs 10 --batch_size 16 --max_turns 2
+# CUDA_VISIBLE_DEVICES=1 python train_model.py \
+#     --train_file "/home/holy/projects/ambiguous-mm-dialogue/preprocessed_data/ambiguous_candidates/simmc2.1_ambiguous_candidates_dstc11_train.json" \
+#     --dev_file "/home/holy/projects/ambiguous-mm-dialogue/preprocessed_data/ambiguous_candidates/simmc2.1_ambiguous_candidates_dstc11_dev.json" \
+#     --devtest_file "/home/holy/projects/ambiguous-mm-dialogue/preprocessed_data/ambiguous_candidates/simmc2.1_ambiguous_candidates_dstc11_devtest.json" \
+#     --result_save_path "/home/holy/projects/ambiguous-mm-dialogue/results/ambiguous-candidates/detr/bert" \
+#     --visual_feature_path "/home/holy/projects/ambiguous-mm-dialogue/preprocessed_data/visual_features/visual_features_detr.pt" \
+#     --visual_feature_size 260 \
+# 	--backbone bert --use_gpu --num_epochs 10 --batch_size 16 --max_turns 2
